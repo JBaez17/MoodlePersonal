304f0a38c1e5595ed753c01ebae62935c9c3e7d2
super usuario vagrant <vagrant@vagrant.vagrant-debian-amd64>
2015-12-07 13:58:36 -0600

    Se agregaron las pruebas BDD para la creacion del super usuario

--- MoodlePersonal/settings.py
+++ MoodlePersonal/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'MoodlePersonal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'moodle',
+        'USER': 'moodle',
+        'PASSWORD': 'moodle',
+        'PORT': '3306',
+        'HOST': '127.0.0.1',
     }
 }
 
@@ -85,7 +89,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
 TIME_ZONE = 'UTC'
 
