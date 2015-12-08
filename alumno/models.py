03e935d6d3fda2a9fb644eba21f1e5cba1e47271
super usuario vagrant <vagrant@vagrant.vagrant-debian-amd64>
2015-12-07 21:23:06 -0600

    modelo alumno

--- alumno/models.py
+++ alumno/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Alumno(models.Model):
+
+    username = models.CharField(max_length = 25)
+    password = models.CharField(max_length = 25)
+    nombre = models.CharField(max_length = 65)
+    apellidos = models.CharField('Apellidos',max_length = 120)
+    mail = models.EmailField('E-mail', max_length=60)
+
+    def __str__(self):
+        return self.nombre
